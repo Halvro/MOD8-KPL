a2ffaa3752156528bbddbd3694aae6f6e452faa2
unknown <haikalalfaro123@gmail.com>
2024-04-20 09:07:04 +0700

    aplikasi bank transfer dengan runtime configuration

--- modul8_1302220098/Program.cs
+++ modul8_1302220098/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace modul8_1302220098
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            UIConfig uiConfig = new UIConfig();
+            int jumlahTF, biayaTF;
+            string metode, confirm;
+            if (uiConfig.bankConfig.lang == "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer: ");
+                jumlahTF = Convert.ToInt32(Console.ReadLine());
+                if (jumlahTF <= uiConfig.bankConfig.transfer.treshold)
+                {
+                    biayaTF = uiConfig.bankConfig.transfer.low_fee;
+                }
+                else
+                {
+                    biayaTF = uiConfig.bankConfig.transfer.high_fee;
+                }
+                Console.WriteLine($"Transfer fee = {biayaTF}");
+                Console.WriteLine($"Total amount = {jumlahTF + biayaTF}");
+                Console.WriteLine("Select transfer method: ");
+                for (int i = 0; i < uiConfig.bankConfig.method.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {uiConfig.bankConfig.method[i]}");
+                }
+            }
+        }
+    }
+}
---
+++ modul8_1302220098/UIConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace modul8_1302220098
+{
+    internal class UIConfig
+    {
+        public UIConfig()
+        {
+            try
+            {
+                ReadConfig();
+            }
+            catch
+            {
+                SetDefault();
+                WriteConfig();
+            }
+        }
+        public BankTransferConfig bankConfig;
+        public void ReadConfig()
+        {
+            string text = File.ReadAllText("C:\\Users\\Haikal\\OneDrive\\Documents\\Konstruksi Perangkat Lunak\\modul8_1302220098\\modul8_1302220098\\bank_transfer_config.json");
+            bankConfig = JsonSerializer.Deserialize<BankTransferConfig>(text);
+        }
+
+        public void WriteConfig()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(bankConfig, options);
+            File.WriteAllText("C:\\Users\\Haikal\\OneDrive\\Documents\\Konstruksi Perangkat Lunak\\modul8_1302220098\\modul8_1302220098\\bank_transfer_config.json", jsonString);
+        }
+
+        public void SetDefault()
+        {
+            List<string> method = new List<string> { "RTO(real - time)", "SKN", "RTGS", "BI FAST" };
+            bankConfig = new BankTransferConfig("en", 25000000, 6500, 15000, method, "yes", "ya");
+        }
+        public void ChangeLang()
+        {
+            if(bankConfig.lang == "en")
+            {
+                bankConfig.lang = "id";
+                WriteConfig();
+            }
+            else
+            {
+                bankConfig.lang = "en";
+                WriteConfig();
+            }
+        }
+    }
+}
